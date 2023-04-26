38b349f619cadb4e7bd7bf6ecd04be882a6c0f2d
Gilbo11 <jackgj43@icloud.com>
2023-04-26 14:57:48 +0100

    form validation

--- javascript/contact_form.js
+++ javascript/contact_form.js
@@ -1,3 +1,20 @@
 document.getElementById("contact_form").addEventListener("submit", e=>{
-    
-})
+    let nameinput = document.forms["contact_form"] ["textname"].value;
+    let email = document.forms["contact_form"] ["Email"].value;
+    let message = document.forms["contact_form"] ["message"].value;
+
+    if(nameinput.trim() == ""){
+        e.preventDefault();
+        alert("Please enter a name.");
+    }
+
+    if(email.trim() == ""){
+        e.preventDefault();
+        alert("Please enter an Email address.");
+    }
+
+    if(message.trim() == ""){
+        e.preventDefault();
+        alert("Please enter a message if you wish to contact us.");
+    }
+});
